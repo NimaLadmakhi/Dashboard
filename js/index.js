f9754f35758a7b20276c1ef84e0032397d57205b
cody <cody18v@gmail.com>
2021-03-13 03:18:17 -0800

    add responsive

--- js/index.js
+++ js/index.js
@@ -1,12 +1,25 @@
 $( document ).ready( function () {
   $( ".main__bar" ).click( function () {
+    if ( window.innerWidth >= 1000 && window.innerWidth <= 1300 ) {
+      $( ".aside" ).animate( { width: '45%' } );
+      $( ".aside__text" ).css( { display: 'inline-block' } )
+      $( ".aside__close" ).css( { display: 'inline-block' } )
+    } else {
+      $( ".aside" ).animate( { width: '60%' } );
+      $( ".aside__text" ).css( { display: 'none' } )
+    }
     $( ".main" ).addClass( 'main--blur' );
     $( "body" ).css( { overflow: 'hidden' } )
-    $( ".aside" ).animate( { width: '60%' } );
   } );
   $( ".aside__close" ).click( function () {
-    $( ".main" ).removeClass( 'main--blur' );
+    if ( window.innerWidth >= 1000 && window.innerWidth <= 1300 ) {
+      $( ".aside" ).animate( { width: '10%' } );
+      $( ".aside__text" ).css( { display: 'none' } )
+      $( ".aside__close" ).css( { display: 'none' } )
+    } else {
+      $( ".aside" ).animate( { width: '0' } );
+    }
+      $( ".main" ).removeClass( 'main--blur' );
     $( "body" ).css( { overflow: 'auto' } )
-    $( ".aside" ).animate( { width: '0' } );
   } );
 } );
